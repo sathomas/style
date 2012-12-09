c3e3a77a5f3c05a0c8bc257eb193eb9001337e74
Stephen A Thomas <stephen@sathomas.me>
2012-12-09 10:24:11 -0500

    Initial commit for style scaffolding

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+/*
+ * Master file that defines individual
+ * script files and their order for
+ * concatenation and minification.
+ */
+
+//@codekit-prepend "jquery-1.8.3.js"
+//@codekit-prepend "bootstrap-transition.js"
+//@codekit-prepend "bootstrap-alert.js"
+//@codekit-prepend "bootstrap-modal.js"
+//@codekit-prepend "bootstrap-dropdown.js"
+//@codekit-prepend "bootstrap-scrollspy.js"
+//@codekit-prepend "bootstrap-tab.js"
+//@codekit-prepend "bootstrap-tooltip.js"
+//@codekit-prepend "bootstrap-popover.js"
+//@codekit-prepend "bootstrap-button.js"
+//@codekit-prepend "bootstrap-collapse.js"
+//@codekit-prepend "bootstrap-carousel.js"
+//@codekit-prepend "bootstrap-typeahead.js"
+//@codekit-prepend "bootstrap-affix.js"
+//@codekit-prepend "prism.js"
